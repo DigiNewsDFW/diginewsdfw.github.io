7506f20bbd62c88653e2fae65bec4a708e0cb94b
Danielle Abril <danielle.m.abril@gmail.com>
2018-10-24 21:55:39 -0500

    Added Kaplan/Crypto story

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,12 +1,20 @@
 const articles = [{
+  title: 'Dallas Fed\'s Rob Kaplan weighs in on cryptocurrencies and AI',
+  date: 'October 25, 2018 9:00 AM',
+  blurb: 'The CEO is keeping an eye on the emerging technologies and the increased pressure it will add to the local workforce.',
+  description: '<p>Rob Kaplan, CEO of the Federal Reserve Bank of Dallas, is keeping a close eye on the traction of cryptocurrency, blockchain and artificial intelligence and what role it will play in the economy of the future. While he\'s skeptical about cryptocurrencies in their current form, he\'s much more certain that emerging technologies like blockchain and artificial intelligence will transform industries and put pressure on the workforce.</p><p>"My guess is we look back 20 years from now, and [blockchain] is going to be used by a range of industries we can\'t imagine right now," he said. "It could be very powerful."</p><p>Kaplan was one of three speakers featured on a panel titled "Cryptocurrencies, AI, and Disruptive Technologies That Will Redefine the Global Economy." The panel took place at the George W. Bush Presidential Center on Oct. 22 and was moderated by Ken Hersh, CEO of the center. Tuur Demeester, founder of Ukraine-based bitcoin fund Adamant Capital, and Barry Eichengreen, University of California-Berkley professor of economics and political science and a former senior advisor at the International Monetary Fund, also spoke on the panel.</p>',
+  page: '2018-10-25-dallas-feds-rob-kaplan-on-cryptocurrencies-and-ai.html',
+  image: '2018-10-25-dallas-feds-rob-kaplan-on-cryptocurrencies-and-ai.jpg',
+  author: 'Danielle Abril',
+  featured: true
+}, {
   title: 'Young venture capitalists plant roots in Dallas to invest in early stage startups',
   date: 'October 18, 2018 3:30 PM',
   blurb: 'TXV Partners aims to specialize in startups disrupting Millennial and Gen Z markets.',
   description: '<p>A group of young venture capitalists are hoping to infuse the North Texas startup community with early stage funding focusing on companies disrupting Millennial and Generation Z markets.</p><p>TXV Partners, founded in Dallas in April, expects to close its $50 million fund by the end of the year. Led by managing partners Marcus Stroud, Brandon Allen and Shawn Narosov, the firm aims to invest in companies specializing in fintech, blockchain, esports or those that are providing new tech platforms or are consumer-based. The firm\'s founders, all age 25 years and younger, plan to leverage companies catering to younger consumers, particularly Millennials or Generation Z, which range in age from 8 to about 37.</p><p>While the firm expects to invest across the U.S. and even globally, it has particular interest in the local region.</p><p>"We want to continue to grow the venture scene in Texas, especially North Texas," Stroud said. "We want to be at the forefront of that push."</p>',
   page: '2018-10-18-txv-partners-investing-in-north-texas-startups.html',
   image: '2018-10-18-txv-partners-investing-in-north-texas-startups.jpg',
-  author: 'Danielle Abril',
-  featured: true
+  author: 'Danielle Abril'
 }, {
   title: 'DART to finance rides to and from transit stations in 2019',
   date: 'October 11, 2018 7:00 AM',
